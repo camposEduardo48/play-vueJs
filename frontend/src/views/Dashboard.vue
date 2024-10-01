a4eeec77eca219afaecfe76ed4deca7c9834975a
camposEduardo48 <oicedu48@gmail.com>
2024-10-01 14:39:52 -0300

    feat: new style of list on dashboard page

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -50,7 +50,7 @@
                 </v-card>
               </v-col>
               <v-col class="d-flex flex-column ga-4">
-                <v-card elevation="20">
+                <v-card elevation="10">
                   <v-card-text class="d-flex align-center ga-1">
                     <span>
                       <h2>{{ `company_name` }}</h2>
@@ -61,7 +61,7 @@
                   <v-row class="pa-4">
                     <v-col class="d-flex ga-4">
                       <v-btn text="Say Hello" @click="() => sendMessage()"></v-btn>
-                      <v-dialog max-width="800px">
+                      <v-dialog max-width="500px">
                         <template v-slot:activator="{ props: activatorProps }">
                           <v-btn text="Adicionar novo objeto" v-bind="activatorProps" @click="() => createNewObject()"></v-btn>
                         </template>
@@ -80,17 +80,23 @@
                                   natus officiis eveniet ab quos?
                                 </v-card-text>
                               </v-card>
-                              <v-sheet>
+                              <v-card>
                                 <v-form @submit.prevent="() => addNewObject()" class="pa-6">
                                   <v-select
                                     v-model="type_object.value"
                                     :items="type_object"
-                                    label="Selecione o tipo de objeto"
+                                    label="Selecione o tipo de tarefa"
                                     required
                                   ></v-select>
-                                  <v-text-field v-model="name_object" label="Nome do objeto" required></v-text-field>
-                                  <v-text-field v-model="detail_object" label="Descrição do objeto" required></v-text-field>
-                                  <v-checkbox label="Desejar manter ativo no sistema?"></v-checkbox>
+                                  <v-select
+                                    v-model="color.value"
+                                    :items="color"
+                                    label="Selecione a cor do card"
+                                    disabled
+                                  ></v-select>
+                                  <v-text-field v-model="name_object" label="Titulo da tarefa" required></v-text-field>
+                                  <v-text-field v-model="detail_object" label="Descreva a tarefa" required></v-text-field>
+                                  <v-checkbox v-model="priority" label="Prioridade para concluir essa tarefa?"></v-checkbox>
                                   <v-card-actions>
                                     <v-row class="d-flex justify-end">
                                       <v-btn variant="plain" text="Cancelar" @click="isActive.value = false"></v-btn>
@@ -98,7 +104,7 @@
                                     </v-row>
                                   </v-card-actions>
                                 </v-form>
-                              </v-sheet>
+                              </v-card>
                             </v-col>
                           </v-row>
                         </template>
@@ -106,32 +112,44 @@
                     </v-col>
                   </v-row>
                 </v-card>
-                <v-row>
-                  <v-col>
-                    <v-card class="overflow-y-auto custom-scrollbar absolute" elevation="20" max-height="500px">
-                      <v-card-text class="custom-z-index w-full">
-                        <h3 block>Products</h3>
-                      </v-card-text>
-                      <v-divider> </v-divider>
-                      <v-table class="mt-14 w-full">
-                        <thead>
-                          <tr>
-                            <th>service</th>
-                            <th>company</th>
-                            <th>status</th>
-                            <th>date</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr v-for="object in object_info" :key="object.id">
-                            <td>{{ object.type }}</td>
-                            <td>{{ object.name }}</td>
-                            <td>{{ object.detail }}</td>
-                            <td>{{ object.name }}</td>
-                          </tr>
-                        </tbody>
-                      </v-table>
-                    </v-card>
+                <v-row class="d-flex justify-space-between ma-0">
+                  <v-col class="d-flex flex-column">
+                    <ul v-for="object in object_info" :key="object.id" class="pa-4">
+                      <li>
+                        <b>{{ object.type }}</b>
+                      </li>
+                      <v-divider></v-divider>
+                      <li>{{ object.name }}</li>
+                      <li>{{ object.detail }}</li>
+                      <li>
+                        <b>{{ object.priority === true ? 'Importante' : null }}</b>
+                      </li>
+                      <li class="d-flex justify-space-between">
+                        <v-btn>
+                          <PhPencilLine :size="24" />
+                        </v-btn>
+                        <v-btn>
+                          <PhTrash :size="24" />
+                        </v-btn>
+                        <v-btn>
+                          <PhArrowSquareRight :size="24" />
+                        </v-btn>
+                      </li>
+                    </ul>
+                  </v-col>
+                  <v-col class="d-flex flex-column">
+                    <ul class="pa-4">
+                      <li>
+                        <b>Em execução</b>
+                      </li>
+                    </ul>
+                  </v-col>
+                  <v-col class="d-flex flex-column">
+                    <ul class="pa-4">
+                      <li>
+                        <b>Finalizado</b>
+                      </li>
+                    </ul>
                   </v-col>
                 </v-row>
               </v-col>
@@ -144,156 +162,182 @@
   </Layout>
 </template>
 <script setup>
-  import Container from '@/components/container/Container.vue'
-  import Footer from '@/components/footer/Footer.vue'
-  import Header from '@/components/header/Header.vue'
-  import Layout from '@/components/layout/Layout.vue'
-  import axios from 'axios'
-  import { io } from 'socket.io-client'
-  import { onMounted, onUnmounted, ref } from 'vue'
-  import { toast } from 'vue3-toastify'
-  import background from './../assets/bg-dashboard.svg'
+import Container from '@/components/container/Container.vue'
+import Footer from '@/components/footer/Footer.vue'
+import Header from '@/components/header/Header.vue'
+import Layout from '@/components/layout/Layout.vue'
+import { PhArrowSquareRight, PhPencilLine, PhTrash } from '@phosphor-icons/vue'
+import axios from 'axios'
+import { io } from 'socket.io-client'
+import { onMounted, onUnmounted, ref } from 'vue'
+import { toast } from 'vue3-toastify'
+import background from './../assets/bg-dashboard.svg'
 
-  const status_test = ref(true)
+const status_test = ref(true)
+const img_background = background
+const apiJson = 'http://localhost:3300'
+const user_info = ref('')
+const object_info = ref([])
+const user_nickname = ref('')
+const type_object = ref(['Lazer', 'Trabalho', 'Hobby'])
+const color = ref(['Azul', 'Vermelho', 'Verde', 'Amarelo'])
+const name_object = ref('')
+const detail_object = ref('')
+const color_task = ''
+const priority = ref(false)
 
-  const img_background = background
-  const apiJson = 'http://localhost:3300'
-  const user_info = ref('')
-  const object_info = ref([])
-  const user_nickname = ref('')
-  const type_object = ref(['fisico', 'virtual'])
-  const name_object = ref('')
-  const detail_object = ref('')
+let socket
 
-  let socket
+const newItem = () => {
+  toast.success('Adicionado com sucesso', {
+    autoClose: 2500,
+  }) // ToastOptions
+  return { newItem }
+}
+const invalid = () => {
+  toast.error('Informação inválida', {
+    autoClose: 2000,
+  }) // ToastOptions
+  return { invalid }
+}
 
-  const newItem = () => {
-    toast.success('Adicionado com sucesso', {
-      autoClose: 2500,
-    }) // ToastOptions
-    return { newItem }
-  }
-  const invalid = () => {
-    toast.error('Informação inválida', {
-      autoClose: 2000,
-    }) // ToastOptions
-    return { invalid }
-  }
-
-  const addNewObject = async () => {
-    try {
-      const modelObject = {
-        type: type_object.value.value,
-        name: name_object.value,
-        detail: detail_object.value,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      }
-      type_object.value = ''
-      name_object.value = ''
-      detail_object.value = ''
-      if (name_object.value.length > 3 && detail_object.value.length > 5) {
-        newItem()
-        const request = await axios.post(`${apiJson}/objects`, modelObject)
-        return request
-      }
-      invalid()
-    } catch (err) {
-      console.log(`algo deu errado: ${err}`)
+const addNewObject = async () => {
+  try {
+    const modelObject = {
+      type: type_object.value.value,
+      name: name_object.value,
+      detail: detail_object.value,
+      priority: priority.value,
+      createdAt: new Date(),
+      updatedAt: new Date(),
     }
+    type_object.value = ''
+    name_object.value = ''
+    detail_object.value = ''
+    priority.value = false
+    // if (name_object.value.length != 0 && detail_object.value.length != 0) {
+    //   newItem()
+    //   return request
+    // }
+    const request = await axios.post(`${apiJson}/list`, modelObject)
+    newItem()
+  } catch (err) {
+    console.log(`algo deu errado: ${err}`)
   }
+}
 
-  const connectSocket = () => {
-    socket = io('http://localhost:3000/dashboard')
-    socket.on('connect', () => {
-      console.log(console.log('Conectado com SOCKET.IO'))
-    })
-    socket.on('disconnect', () => {
-      console.log('Desconectado')
-    })
-  }
-
-  const sendMessage = () => {
-    if (socket) {
-      socket.emit('message', 'Olá, servidor via SOCKET.IO!') //verificar SOCKETS
-    }
-    console.log('say hello')
-  }
-  const getObject = async () => {
-    try {
-      const request = await axios.get(`${apiJson}/objects`)
-      object_info.value = request.data
-      const object_datas = object_info.value
-      return console.log(object_datas)
-    } catch (err) {
-      console.log(`algo deu errado: ${err}`)
-    }
-  }
-  const getUser = async () => {
-    if (socket) {
-      socket.emit('received', 'dados recebidos com sucesso! SOCKET.IO')
-    }
-    try {
-      const request = await axios.get(`${apiJson}/users`)
-      user_info.value = request.data
-      const datas = user_info.value
-      const nick = datas.map((item) => item.nickname)
-      user_nickname.value = nick[0]
-    } catch (err) {
-      console.log(`algo deu errado: ${erro}`)
-    }
-  }
-
-  onMounted(() => {
-    connectSocket()
-    getUser()
-    getObject()
+const connectSocket = () => {
+  socket = io('http://localhost:3000/dashboard')
+  socket.on('connect', () => {
+    console.log(console.log('Conectado com SOCKET.IO'))
   })
-  onUnmounted(() => {
-    if (socket) {
-      socket.disconnect()
-    }
+  socket.on('disconnect', () => {
+    console.log('Desconectado')
   })
+}
+
+const sendMessage = () => {
+  if (socket) {
+    socket.emit('message', 'Olá, servidor via SOCKET.IO!') //verificar SOCKETS
+  }
+  console.log('say hello')
+}
+const getObject = async () => {
+  try {
+    const request = await axios.get(`${apiJson}/list`)
+    object_info.value = request.data
+    const object_datas = object_info.value
+    return console.log(object_datas)
+  } catch (err) {
+    console.log(`algo deu errado: ${err}`)
+  }
+}
+const getUser = async () => {
+  if (socket) {
+    socket.emit('received', 'dados recebidos com sucesso! SOCKET.IO')
+  }
+  try {
+    const request = await axios.get(`${apiJson}/users`)
+    user_info.value = request.data
+    const datas = user_info.value
+    const nick = datas.map((item) => item.nickname)
+    user_nickname.value = nick[0]
+  } catch (err) {
+    console.log(`algo deu errado: ${erro}`)
+  }
+}
+
+onMounted(() => {
+  connectSocket()
+  getUser()
+  getObject()
+})
+onUnmounted(() => {
+  if (socket) {
+    socket.disconnect()
+  }
+})
 </script>
 <style scoped>
-  .layout[data-v-c9bfb757] {
-    height: 100%;
-    background-size: contain;
-    background-position: 0%;
-  }
-  div.header {
-    display: flex;
-    justify-content: end;
-    align-items: center;
-  }
-  span.status {
-    height: 10px;
-    width: 10px;
-    border-radius: 100%;
-    background: silver;
-  }
-  .custom-z-index {
-    position: fixed;
-    word-wrap: break-word;
-    z-index: 1;
-    background: #fff;
-    box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.5);
-  }
-  .custom-scrollbar::-webkit-scrollbar {
-    width: 8px; /* Largura da barra de rolagem */
-  }
+.layout[data-v-c9bfb757] {
+  height: 100%;
+  background-size: contain;
+  background-position: 0%;
+}
+div.header {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+}
+span.status {
+  height: 10px;
+  width: 10px;
+  border-radius: 100%;
+  background: silver;
+}
+.v-sheet {
+  background: transparent;
+  box-shadow: none;
+}
+.v-col {
+  color: #000;
+}
+ul {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  justify-content: center;
+  list-style: none;
+  background: #fff;
+  width: 100%;
+  box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.5);
+  margin: 10px 0px 10px 0px;
+  border-radius: 6px;
+}
+svg {
+  color: #874b91;
+}
+.custom-z-index {
+  word-wrap: break-word;
+  background: #fff;
+  border-radius: 4px;
+}
+.custom-scrollbar::-webkit-scrollbar {
+  width: 8px; /* Largura da barra de rolagem */
+}
 
-  .custom-scrollbar::-webkit-scrollbar-track {
-    background-color: #f1f1f1; /* Cor de fundo da trilha da barra de rolagem */
-  }
+.custom-scrollbar::-webkit-scrollbar-track {
+  background-color: #f1f1f1; /* Cor de fundo da trilha da barra de rolagem */
+  border-radius: 10px; /* Arredondamento dos cantos */
+}
 
-  .custom-scrollbar::-webkit-scrollbar-thumb {
-    background-color: #ba68c8; /* Cor da barra de rolagem */
-    border-radius: 10px; /* Arredondamento dos cantos */
-  }
+.custom-scrollbar::-webkit-scrollbar-thumb {
+  background-color: #ba68c8; /* Cor da barra de rolagem */
+  border-radius: 10px; /* Arredondamento dos cantos */
+}
 
-  .custom-scrollbar::-webkit-scrollbar-thumb:hover {
-    cursor: pointer;
-    background-color: #874b91; /* Cor ao passar o mouse */
-  }
+.custom-scrollbar::-webkit-scrollbar-thumb:hover {
+  cursor: pointer;
+  background-color: #874b91; /* Cor ao passar o mouse */
+}
 </style>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -47,84 +47,84 @@
   </Layout>
 </template>
 <script setup>
-  import Container from '@/components/container/Container.vue'
-  import Footer from '@/components/footer/Footer.vue'
-  import Layout from '@/components/layout/Layout.vue'
-  import { PhBiohazard } from '@phosphor-icons/vue'
-  import axios from 'axios'
-  import { onMounted, ref } from 'vue'
-  import { toast } from 'vue3-toastify'
-  import 'vue3-toastify/dist/index.css'
-  import background from './../assets/bg-login.svg'
-  const apiJson = 'http://localhost:3300'
-  const img_background = background
+import Container from '@/components/container/Container.vue'
+import Footer from '@/components/footer/Footer.vue'
+import Layout from '@/components/layout/Layout.vue'
+import { PhBiohazard } from '@phosphor-icons/vue'
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+import { toast } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+import background from './../assets/bg-login.svg'
+const apiJson = 'http://localhost:3300'
+const img_background = background
 
-  const user_nickname = ref('')
-  const user_email = ref('')
-  const submitted = ref(false)
-  const show_users = ref([])
+const user_nickname = ref('')
+const user_email = ref('')
+const submitted = ref(false)
+const show_users = ref([])
 
-  // const notify = () => {
-  //   toast('Dados enviados com sucesso!', {
-  //     autoClose: 2000,
-  //   }) // ToastOptions
-  //   return { notify }
-  // }
+// const notify = () => {
+//   toast('Dados enviados com sucesso!', {
+//     autoClose: 2000,
+//   }) // ToastOptions
+//   return { notify }
+// }
 
-  const notifyAccess = () => {
-    toast.success('Acesso liberado', {
-      autoClose: 4000,
-    }) // ToastOptions
-    return { notifyAccess }
+const notifyAccess = () => {
+  toast.success('Acesso liberado', {
+    autoClose: 4000,
+  }) // ToastOptions
+  return { notifyAccess }
+}
+
+const invalid = () => {
+  toast.error('Dados inválidos!', {
+    autoClose: 2000,
+  }) // ToastOptions
+  return { invalid }
+}
+const problem = () => {
+  toast.error('Acessado negado', {
+    className: 'custom-style-toastify',
+    autoClose: 2000,
+  }) // ToastOptions
+  return { problem }
+}
+
+const postUser = async () => {
+  const request = await axios.get(`${apiJson}/users`)
+  const reqAll = ref([])
+  reqAll.value = request.data
+  const searchData = reqAll.value
+  const user_datas = {
+    nickname: user_nickname.value.trim(),
+    email: user_email.value.trim(),
+    permissions: false,
+    createdAt: new Date(),
   }
 
-  const invalid = () => {
-    toast.error('Dados inválidos!', {
-      autoClose: 2000,
-    }) // ToastOptions
-    return { invalid }
-  }
-  const problem = () => {
-    toast.error('Acessado negado', {
-      className: 'custom-style-toastify',
-      autoClose: 2000,
-    }) // ToastOptions
-    return { problem }
-  }
+  // localStorage.getItem(user_datas)
 
-  const postUser = async () => {
-    const request = await axios.get(`${apiJson}/users`)
-    const reqAll = ref([])
-    reqAll.value = request.data
-    const searchData = reqAll.value
-    const user_datas = {
-      nickname: user_nickname.value.trim(),
-      email: user_email.value.trim(),
-      permissions: false,
-      createdAt: new Date(),
-    }
+  const req_nick = searchData.find((item) => user_datas.nickname === item.nickname)
+  const req_email = searchData.find((item) => user_datas.email === item.email)
 
-    // localStorage.getItem(user_datas)
-
-    const req_nick = searchData.find((item) => user_datas.nickname === item.nickname)
-    const req_email = searchData.find((item) => user_datas.email === item.email)
-
-    /** verificação para postage de novo usuário!
+  /** verificação para postage de novo usuário!
      * const req_nick = searchData.find(item => user_datas.nickname === item.nickname || user_datas.nickname.length < 6)
       const req_email = searchData.find(item => user_datas.email === item.email || user_datas.email.length < 10)
      */
 
-    try {
-      if (req_nick && req_email) {
-        notifyAccess()
-        setTimeout(() => {
-          const redirectTo = () => {
-            return (window.location.href = '/dashboard')
-          }
-          redirectTo()
-        }, 2000)
-      }
-      /** postar novo usuario
+  try {
+    if (req_nick && req_email) {
+      notifyAccess()
+      setTimeout(() => {
+        const redirectTo = () => {
+          return (window.location.href = '/dashboard')
+        }
+        redirectTo()
+      }, 2000)
+    }
+    /** postar novo usuario
        * if (!req_nick && !req_email) {
 
         const response = await axios.post(`${apiJson}/users`, user_datas)
@@ -132,43 +132,43 @@
         return console.log('Novo usuário adicionado com sucesso!')
       }
        */
-      if (req_nick.length === 0 && req_nick.length < 6) {
-        return invalid()
-      }
-      if (req_email.length === 0 && req_email.length < 8) {
-        return invalid()
-      }
-    } catch (err) {
-      problem()
-      console.log(`Ops, tivemos um problema ao realizar a função! ${err}`)
+    if (req_nick.length === 0 && req_nick.length < 6) {
+      return invalid()
     }
+    if (req_email.length === 0 && req_email.length < 8) {
+      return invalid()
+    }
+  } catch (err) {
+    problem()
+    console.log(`Ops, tivemos um problema ao realizar a função! ${err}`)
   }
+}
 
-  onMounted(() => {})
+onMounted(() => {})
 </script>
 
 <style scoped>
-  .layout[data-v-c9bfb757] {
-    height: 100%;
-    background-size: contain;
-    background-position: 0%;
-  }
-  .v-btn.v-btn--density-default {
-    position: relative;
-  }
+.layout[data-v-c9bfb757] {
+  height: 100%;
+  background-size: contain;
+  background-position: 0%;
+}
+.v-btn.v-btn--density-default {
+  position: relative;
+}
 
-  ul {
-    list-style: none;
-  }
-  .custom-style-toastify {
-    background: #874b91 !important;
-  }
-  svg {
-    color: #874b91;
-  }
-  button {
-    padding: 0.5rem;
-    color: #fff;
-    background: #874b91 !important;
-  }
+ul {
+  list-style: none;
+}
+.custom-style-toastify {
+  background: #874b91 !important;
+}
+svg {
+  color: #874b91;
+}
+button {
+  padding: 0.5rem;
+  color: #fff;
+  background: #874b91 !important;
+}
 </style>
